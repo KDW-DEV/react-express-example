aee99f9a25e5f485b3304e3cd60efe5697b25d0c
Tristan <tristancc99@gmail.com>
2020-04-04 18:33:40 -0400

    init commit

---
+++ build/precache-manifest.77439a778f7ced5625a98b1ce0474331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1b06169937181f023e2a87e203c6bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb6a9a715d38a8b785b9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "349f8400b53d46fbb6f1",
+    "url": "/static/js/2.bc7ad9ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc7ad9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6a9a715d38a8b785b9",
+    "url": "/static/js/main.6a8cf1ac.chunk.js"
+  },
+  {
+    "revision": "e43ff7def22ec7c6b961",
+    "url": "/static/js/runtime-main.a292751d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
